bd8e0dc6d616a963d524c595989f8300eeb89592
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-16 12:50:49 +0200

    add and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('It Works!')
+    res.render('home', { layout: false });
 });
 
 app.listen(6969, () => console.log('Server is listening on http://localhost:6969...'));
