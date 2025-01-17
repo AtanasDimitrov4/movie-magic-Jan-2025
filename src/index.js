94880870087c60a1882a8d7a4bebac60804a1b1f
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-17 11:33:51 +0200

    add home controller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import express from 'express';
 import handlebars from 'express-handlebars';
-
+import homeController from './controllers/home-controller.js';
+    
 
 const app = express();
 
@@ -14,13 +15,8 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(homeController);
 
-app.get('/about', (req, res) =>{
-    res.render('about');
-});
 
 app.get('*', (req, res) => {
     res.render('404');
