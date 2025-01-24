e8ab021f12de589459e0adf805c358a99015d6fb
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-24 13:32:43 +0200

    refactor details page to use database

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -22,9 +22,9 @@ export default {
         return Movie.find({});
     },
    
-    findOne(movieId){
-          // TODO: if movie is missing?
-       const result = movies.find(movie => movie.id === movieId);
+    getOne(movieId){
+        
+       const result = Movie.findById(movieId);
 
        return result;
     },
