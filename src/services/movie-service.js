f2abcb8c16c9e797182aebe9c0e661600395f9d1
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-25 13:27:33 +0200

    temporary fix search page

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -5,21 +5,24 @@ import Movie from '../models/Movie.js';
 
 export default {
     getAll(filter = {}) {
-        //let result = Movie.find({});
+        let query = Movie.find({});
 
-      //  if(filter.search) {
-      //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-      //  }
+        if(filter.search) {
+            //TODO: fix partial case insensetive search
+           query = query.find({title: filter.search});
+        }
 
-      //  if(filter.genre){
-      //     result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
-      //  }
+        if(filter.genre){
+            //TODO: add case insensetive search
+          query = query.find({ genre: filter.genre});
+        }
 
-      //  if(filter.year) {
-      //      result = result.filter(movie => movie.year === filter.year)
-      //  }
+        if(filter.year) {
+            query = query.find({year: Number(filter.year)});
+            
+        }
        
-        return Movie.find({});
+        return query;
     },
    
     getOne(movieId){
