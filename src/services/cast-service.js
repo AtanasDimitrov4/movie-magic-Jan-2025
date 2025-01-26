3c868c802e1563c396225282fb197009af8ff59a
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-26 14:04:43 +0200

    show cast select in attach page

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,8 +1,11 @@
 import Cast from "../models/Cast.js";
 
 export default {
+    getAll() {
+        return Cast.find({});
+    },
     create(castData) {
-       //TODO: Create cast
+      
        return Cast.create(castData);
       
     }
