6da1a994195fc7d46261f345e884fe0a9ca887d3
Atanas Dimitrov4 <ataldi@abv.bg>
2025-01-31 11:59:48 +0200

    hash password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 import { Schema, model } from "mongoose";
+import bcryptjs from 'bcryptjs';
 
 const userSchema = new Schema({
    email: String,
    password: String,
+});
+
+userSchema.pre('save', async function(){
+    //TODO: fix update user bug
+    this.password = await bcryptjs.hash(this.password, 10);
 });
 
 const User = model('User', userSchema);
